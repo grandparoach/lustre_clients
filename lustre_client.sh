65a061bfd96637318bce2957f465f7332f284fce
Steven Roach <stevenwroach@hotmail.com>
2018-05-25 22:09:19 -0700

    Update lustre_client.sh

--- lustre_client.sh
+++ lustre_client.sh
@@ -9,8 +9,6 @@ echo $MASTER_NAME
 
 # Shares
 SHARE_HOME=/share/home
-NFS_ON_MASTER=/share/data
-NFS_MOUNT=/share/data
 
 mkdir -p /share
 mkdir -p $SHARE_HOME
@@ -21,43 +19,23 @@ HPC_UID=7007
 HPC_GROUP=hpc
 HPC_GID=7007
 
-
-mount_nfs()
-{
-
-	yum -y install nfs-utils nfs-utils-lib
-	
-	mkdir -p ${NFS_MOUNT}
-
-	showmount -e ${MASTER_NAME}
-	mount -t nfs ${MASTER_NAME}:${NFS_ON_MASTER} ${NFS_MOUNT}
-	
-	echo "${MASTER_NAME}:${NFS_ON_MASTER} ${NFS_MOUNT} nfs defaults,nofail  0 0" >> /etc/fstab
-}
+yum -y install nfs-utils nfs-utils-lib
 
 
-setup_user()
-{  
-
-	echo "$MASTER_NAME:$SHARE_HOME $SHARE_HOME    nfs4    rw,auto,_netdev 0 0" >> /etc/fstab
-	mount -a
-	mount
+echo "$MASTER_NAME:$SHARE_HOME $SHARE_HOME    nfs4    rw,auto,_netdev 0 0" >> /etc/fstab
+mount -a
+mount
    
-    groupadd -g $HPC_GID $HPC_GROUP
+groupadd -g $HPC_GID $HPC_GROUP
 
-    # Don't require password for HPC user sudo
-    echo "$HPC_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+# Don't require password for HPC user sudo
+echo "$HPC_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
     
-    # Disable tty requirement for sudo
-    sed -i 's/^Defaults[ ]*requiretty/# Defaults requiretty/g' /etc/sudoers
+# Disable tty requirement for sudo
+sed -i 's/^Defaults[ ]*requiretty/# Defaults requiretty/g' /etc/sudoers
 
-	useradd -c "HPC User" -g $HPC_GROUP -d $SHARE_HOME/$HPC_USER -s /bin/bash -u $HPC_UID $HPC_USER
+useradd -c "HPC User" -g $HPC_GROUP -d $SHARE_HOME/$HPC_USER -s /bin/bash -u $HPC_UID $HPC_USER
 
-    chown $HPC_USER:$HPC_GROUP $SHARE_DATA	
-}
-
-
-mount_nfs
-setup_user
+chown $HPC_USER:$HPC_GROUP $SHARE_DATA	
 
 exit 0
